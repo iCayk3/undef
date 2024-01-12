a3f1450e9cf269cfd4d109a8075306db21fbfb9b
Cayke Silva <silvacayke@gmail.com>
2024-01-12 20:25:09 -0300

    subindo o projeto para o github

---
+++ src/main/java/com/wake4solution/undefineName/UndefineNameApplication.java
@@ -0,0 +1,13 @@
+package com.wake4solution.undefineName;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UndefineNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UndefineNameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wake4solution/undefineName/UndefineNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wake4solution.undefineName;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UndefineNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
